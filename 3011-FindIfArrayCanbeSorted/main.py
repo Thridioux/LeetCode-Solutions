7f8651d76f357f24d52d0ea4c5168cfbbb6e0736
Thridioux <donotdisturbyo343@gmail.com>
2024-11-06 10:27:36 +0300

    Problem-3011

---
+++ 3011-FindIfArrayCanbeSorted/main.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def canSortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def count_bits(n):
+            return bin(n).count('1')
+        
+        cur_min , cur_max =nums[0], nums[0]
+        prev_max =float('-inf')
+        
+        for n in nums:
+            if count_bits(n) == count_bits(cur_min):
+                cur_min = min(cur_min, n)
+                cur_max = max(cur_max, n)
+                
+            else:
+                if cur_min < prev_max:
+                    return False
+                
+                prev_max = cur_max
+                cur_min , cur_max = n, n
+        if cur_min < prev_max:
+            return False
+        return True   
