5ac41c8e924fc201337c4ad27ce38f2d6023b544
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-01-02 10:33:53 +0300

    Problem-2559

--- 2559-CountVowelStringsInRanges/main.py
+++ 2559-CountVowelStringsInRanges/main.py
@@ -2,3 +2,16 @@ class Solution:
     def vowelStrings(self, words, queries):
         vowels = set('aeiou')
         
+        prefix_count = [0] * (len(words) + 1)
+        prev = 0
+        for i,w in enumerate(words):
+            if w[0] in vowels and w[-1] in vowels:
+                prev += 1
+            prefix_count[i+1] = prev
+            
+        res = [0] * len(queries)
+        
+        for i,q in enumerate(queries):
+            l,r = q
+            res[i] = prefix_count[r+1] - prefix_count[l]
+        return res
