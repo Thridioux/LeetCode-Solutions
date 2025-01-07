895b08d1dcfda4890bd2d719667ce2e07579a6e7
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-01-07 11:18:32 +0300

    Problem-1408

---
+++ 1408-StringMatchingInArray/main.py
@@ -0,0 +1,15 @@
+class Solution:
+    def stringMatching(self, words):
+        res = []
+        
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i == j:
+                    continue
+                
+                if words[i] in words[j]:
+                    res.append(words[i])
+                    break
+                
+        return res
+        
