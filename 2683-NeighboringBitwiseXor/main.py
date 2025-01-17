9e9a292ccfd347166660dbd98b9d8e25f4edbf8a
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-01-17 14:59:33 +0300

    Problem-2683

---
+++ 2683-NeighboringBitwiseXor/main.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def doesValidArrayExist(self, derived):
+        """
+        :type derived: List[int]
+        :rtype: bool
+        """
+        first = 0
+        last = 0
+        
+        for n in derived:
+            if n:
+                last = ~last
+            
+        return first == last
+    
+        
+        
