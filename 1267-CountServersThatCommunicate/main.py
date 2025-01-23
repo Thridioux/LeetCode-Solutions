54ad268989d011cad5c1f6a717df07ae6382af4d
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-01-23 09:25:26 +0300

    Problem-1267-modified

--- 1267-CountServersThatCommunicate/main.py
+++ 1267-CountServersThatCommunicate/main.py
@@ -6,3 +6,21 @@ class Solution(object):
         """
         ROWS, COLS = len(grid), len(grid[0])
         
+        row_cnt = [0] * ROWS
+        col_cnt = [0] * COLS
+        
+        #preprocess
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    row_cnt[r] += 1
+                    col_cnt[c] += 1
+        
+        res = 0   
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (row_cnt[r] > 1 or col_cnt[c] > 1):
+                    res += 1
+                    
+        return res
+        
