ad04990965568538a710236225aab4629a8110ab
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-01-28 09:45:56 +0300

    Problem-2658

---
+++ 2658-MaxNumberOfFishInGrid/main.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findMaxFish(self, grid):
+        ROWS,COLS = len(grid), len(grid[0])
+        res = 0
+        visit = set()
+        
+        def dfs(r,c):
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS or grid[r][c] == 0 or (r,c) in visit:
+                return 0
+            visit.add((r,c))
+            res = grid[r][c]
+            neighbors = [[r+1,c],[r-1,c],[r,c+1],[r,c-1]]
+            for nr,nc in neighbors:
+                res += dfs(nr,nc)
+            return res  
+        
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                res = max(res, dfs(r,c))
+        
+        return res
+        
