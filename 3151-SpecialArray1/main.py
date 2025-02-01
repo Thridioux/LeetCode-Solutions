a601a7c75007fe4628d5e6f0f2503bc301e201dc
Erdem <erdem@192.168.1.120>
2025-02-01 22:11:38 +0300

    Problem-3151

---
+++ 3151-SpecialArray1/main.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isArraySpecial(self, nums):
+        N = len(nums)
+        if N == 1:
+            return True
+        for i in range(1, N):
+            if nums[i] % 2 == nums[i - 1] % 2:
+                return False
+        return True
+        
