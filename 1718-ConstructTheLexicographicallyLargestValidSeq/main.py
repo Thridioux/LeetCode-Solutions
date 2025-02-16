7378fd24a6250864ad28e1b37b712ecff479e9c4
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-02-16 17:57:59 +0300

    Problem-1718

---
+++ 1718-ConstructTheLexicographicallyLargestValidSeq/main.py
@@ -0,0 +1,43 @@
+class Solution:
+    def constructDistancedSequence(self, n):
+        
+        res = [0] * (2 * n - 1)
+        used = set()
+        
+        def backtrack(i):
+            if i == len(res):
+                return True
+            
+            #try largest elements first
+            
+            for num in reversed(range(1,n + 1)):
+                #validation
+                if num in used:
+                    continue
+                if num != 1 and (i + num >= len(res) or res[i + num]):
+                    continue
+                
+                #try desicion
+                used.add(num)
+                res[i] = num
+                if num > 1:
+                    res[i + num] = num
+                
+                j = i + 1
+                while j<len(res) and res[j]:
+                    j += 1
+                    
+                #recursive step
+                if backtrack(j):
+                    return True
+                
+                #Backtrack
+                used.remove(num)
+                res[i] = 0
+                if num > 1:
+                    res[i + num] = 0
+            return False
+        backtrack(0)
+        
+        return res
+        
