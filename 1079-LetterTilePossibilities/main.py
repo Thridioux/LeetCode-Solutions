b0a9edfea756dcfbc8bf94672f35501c66c2f72a
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-02-17 10:30:34 +0300

    Problem-1079

---
+++ 1079-LetterTilePossibilities/main.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numTilePossibilities(self, tiles):
+        from collections import Counter
+        count = Counter(tiles) #char --> available count
+        
+        def backtrack():
+            res = 0
+            for c in count:
+                if count[c] > 0:
+                    count[c] -= 1
+                    res += 1
+                    res += backtrack()
+                    count[c] += 1
+                    
+            return res
+        
+        return backtrack()
+        
+                
+                    
