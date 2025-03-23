672c2b14789265832a2850e8543c65100d491636
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-03-23 19:08:30 +0300

    Problem-1976

---
+++ 1976-NumberOfWaysToArriveAtDestination/main.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countPaths(self, n, roads):
+        from collections import defaultdict
+        import heapq
+        adj = defaultdict(list)
+        for u, v, w in roads:
+            adj[u].append((w, v))
+            adj[v].append((w, u))
+            
+        MOD = 10**9 + 7
+        min_heap = [(0, 0)] #cost, node
+        min_cost = [float('inf')] * n
+        path_count = [0] * n
+        path_count[0] = 1
+        
+        while min_heap:
+            cost, node = heapq.heappop(min_heap)
+            
+            for nei_cost, nei in adj[node]:
+                if cost + nei_cost < min_cost[nei]:
+                    min_cost[nei] = cost + nei_cost
+                    path_count[nei] = path_count[node]
+                    heapq.heappush(min_heap, (cost + nei_cost, nei))
+                elif cost + nei_cost == min_cost[nei]:
+                    path_count[nei] = (path_count[nei] + path_count[node]) % MOD
+        
+        return path_count[n-1]
