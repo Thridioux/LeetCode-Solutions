f540328fe39f7d5c494a3f7e4be404886d858b26
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-05-07 22:43:21 +0300

    Problem-3341

--- 3341-FindMinTimeToReachLastRoom1/main.py
+++ 3341-FindMinTimeToReachLastRoom1/main.py
@@ -1,18 +1,41 @@
-class Solution(object):
+from collections import deque
+import heapq
+class State:
+    def __init__(self, x, y, dis):
+        self.x = x
+        self.y = y
+        self.dis = dis
+
+    def __lt__(self, other):
+        return self.dis < other.dis
+
+
+class Solution:
     def minTimeToReach(self, moveTime):
-        """
-        :type moveTime: List[List[int]]
-        :rtype: int
-        """
-        m, n = len(moveTime), len(moveTime[0])
-        dp = [[float('inf')] * n for _ in range(m)]
-        dp[0][0] = 0 #start with no cost
+        n = len(moveTime)
+        m = len(moveTime[0])
+        inf = float("inf")
+        d = [[inf] * m for _ in range(n)]
+        v = [[0] * m for _ in range(n)]
 
-        for i in range(m):
-            for j in range(n):
-                if i + 1 < m:
-                    dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + moveTime[i][j])
-                if j + 1 < n:
-                    dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + moveTime[i][j])
+        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
-        return dp[m - 1][n - 1]
+        d[0][0] = 0
+        q = []
+        heapq.heappush(q, State(0, 0, 0))
+
+        while q:
+            s = heapq.heappop(q)
+            if v[s.x][s.y]:
+                continue
+            v[s.x][s.y] = 1
+            for dx, dy in dirs:
+                nx, ny = s.x + dx, s.y + dy
+                if not (0 <= nx < n and 0 <= ny < m):
+                    continue
+                dist = max(d[s.x][s.y], moveTime[nx][ny]) + 1
+                if d[nx][ny] > dist:
+                    d[nx][ny] = dist
+                    heapq.heappush(q, State(nx, ny, dist))
+
+        return d[n - 1][m - 1]
