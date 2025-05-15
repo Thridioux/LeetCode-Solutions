886b019b7b2cb7d1ad489144fed20d98ed6bbd25
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-05-15 22:33:12 +0300

    Problem-2900

--- 2900-LongestUnequalAdjacentGroupsSubsequence1/main.py
+++ 2900-LongestUnequalAdjacentGroupsSubsequence1/main.py
@@ -1,5 +1,17 @@
 class Solution:
     def getLongestSubsequence(self, words, groups):
+        result_subsequence = []
+        if not words:
+            return []
+        result_subsequence.append(words[0])
+        last_added_group_value = groups[0]
         
+        for i in range(1, len(words)):
+            current_group_value = groups[i]
+            if current_group_value != last_added_group_value:
+                result_subsequence.append(words[i])
+                last_added_group_value = current_group_value
+                
+        return result_subsequence
         
         
