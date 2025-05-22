7746a7a613bcce7cc0ad79b150b8598fdbca5f72
Erdem <erdem@Erdems-MacBook-Pro.local>
2025-05-23 00:28:41 +0300

    Problem-3362

---
+++ 3362-ZeroArrayTransformation3/main.py
@@ -0,0 +1,23 @@
+from sortedcontainers import SortedList
+
+class Solution:
+    def maxRemoval(self, nums, queries):
+        n = len(nums)
+        queries.sort(reverse = True)
+        available = SortedList()
+        current = SortedList()
+        
+        for i in range(n):
+            while queries and queries[-1][0] == i:
+                available.add(queries.pop()[1])
+            while current and current[0] < i:
+                current.pop(0)
+            while len(current) < nums[i]:
+                if not available or available[-1] < i:
+                    return -1
+                current.add(available.pop())
+            if len(current) < nums[i]:
+                return -1
+        return len(available)
+
+        
