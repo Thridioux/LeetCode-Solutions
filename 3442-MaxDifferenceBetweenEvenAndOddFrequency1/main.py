ffc78cec93308cde7aecd9db92b17181bd8e31d7
Erdem <erdem@192.168.1.102>
2025-06-10 10:02:21 +0300

    Problem-3442

---
+++ 3442-MaxDifferenceBetweenEvenAndOddFrequency1/main.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        from collections import Counter
+        c = Counter(s)
+        maxOdd = max(x for x in c.values() if x % 2 == 1)
+        minEven = min(x for x in c.values() if x % 2 == 0)
+        return maxOdd - minEven 
+
