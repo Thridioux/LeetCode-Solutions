2aa8008f4be5340183696f56468b66cf002ea855
Thridioux <ferdemcelik@trakya.edu.tr>
2025-06-30 11:26:40 +0300

    Problem-594

---
+++ 594-LongestHarmoniousSubsequence/main.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findLHS(self, nums) -> int:
+        from collections import Counter 
+        count = Counter(nums)
+        max_length = 0
+        for num in count:
+            if num + 1 in count:
+                max_length = max(max_length, count[num] + count[num + 1])
+        return max_length
+    
+        
+        
+        
+        
