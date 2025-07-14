6436178585d1391166c2d4d9be6cbac09bfa6435
Thridioux <ferdemcelik@trakya.edu.tr>
2025-07-14 09:13:29 +0300

    Problem-1290

---
+++ 1290-ConvertBinaryNumberInALinkedList/main.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional, ListNode
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        num = 0
+        while head:
+            num = num * 2 + head.val
+            head = head.next
+        return num
+
+   
+    
+    
+        
