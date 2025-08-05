1e6ee65c5950faa3e7cb441968b67491e363b3f2
Thridioux <ferdemcelik@trakya.edu.tr>
2025-08-05 09:09:10 +0300

    Problem-3477

---
+++ 3477-FruitsIntoBaskets2/main.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def numOfUnplacedFruits(self, fruits: List[int], baskets: List[int]) -> int:
+        count = 0
+        n = len(baskets)
+        for fruit in fruits:
+            unset = 1
+            for i in range(n):
+                if fruit <= baskets[i]:
+                    unset = 0
+                    break
+            count += unset
+            
+        return count
+    
