9b31954c94b1b1912ff8ac4bf5084e6eda5286bd
Thridioux <ferdemcelik@trakya.edu.tr>
2025-09-07 12:45:02 +0300

    Problem-1304

---
+++ 1304-FindNUniqueIntegersSumUpToZero/main.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        #return an array where the values are symmetric around zero
+        ans = []
+        for i in range(1, n//2 + 1):
+            ans.append(i)
+            ans.append(-i)
+        
+        if n % 2 == 1:
+            ans.append(0)
+        
+        return ans
+        
